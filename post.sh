899b22e975c55ac66a922605416a13bd8ebbc93b
Jow1e <63730646+Jow1e@users.noreply.github.com>
2022-12-25 20:20:30 +0300

    Add files via upload

--- post.sh
+++ post.sh
@@ -2,18 +2,20 @@
 set -e
 
 
+# /etc/iwd/main.conf
 sudo systemctl enable --now systemd-resolved 
 sudo ln -srf /run/systemd/resolve/stub-resolv.conf /etc/resolv.conf
 
 
 # checkout patches
-sudo pacman --sync libx11 xorg-server libxrandr xorg-xrandr xorg-xinit libxft xorg-xrdb xorg-xclip unclutter
+# /etc/X11/xorg.conf.d/20-intel.conf
+sudo pacman --sync libx11 xorg-server libxrandr xorg-xrandr xorg-xinit libxft xorg-xrdb xorg-xclip
 
 
 # checkout how to configure it
 sudo pacman --sync pipewire pipewire-alsa pipewire-jack pipewire-pulse wireplumber
 sudo pacman --sync ranger bat tlp htop clang gcc gdb python3 python-poetry zsh
-sudo pacman --sync weechat keepassxc qbittorrent moc zathura feh dunst wireshark-qt nyxt mpv
+sudo pacman --sync weechat keepassxc qbittorrent moc zathura feh dunst wireshark-qt nyxt mpv wireguard-tools texlive-most texlive-most
 
 
 sudo pacman --sync rustup
@@ -31,9 +33,7 @@ sudo pacman --sync gstreamer gst-libav gst-plugins-base gst-plugin-pipewire gst-
 
 
 paru --sync trilium-bin freetube-bin ani-cli librewolf-bin
-paru --sync dwm-git dmenu-git st-git tabbed-git ly-git slock-git
+paru --sync dwm-git dmenu-git st-git tabbed-git ly-git slock-git intel-hybrid-codec-driver
 
 sudo systemctl enable tlp
 sudo systemctl enable ly
-
-sudo btrfs subvolume snapshot / /.snapshots/base
